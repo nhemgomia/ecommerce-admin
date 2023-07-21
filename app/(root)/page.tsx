6424c2f3ae5ac198da5433182dad614ff5f40252
Nehemiah Renel Gomia <nhemgomia@gmail.com>
2023-07-22 07:56:58 +0800

    Add shadcn-ui button and change page.tsx

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,11 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
     return (
-      <p>Hello Admin Dashboard</p>
+      <div className="p-4">
+        <Button size="default" variant="outline">Click Me</Button>
+      </div>
+
     )
   }
   
