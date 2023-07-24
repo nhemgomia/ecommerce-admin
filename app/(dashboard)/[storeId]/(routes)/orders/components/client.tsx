470ae624ad45b65836fa878738aa2dcb58ffb9fa
Nehemiah Renel Gomia <nhemgomia@gmail.com>
2023-07-25 01:22:05 +0800

    Change searchKey for Order data table

--- app/(dashboard)/[storeId]/(routes)/orders/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/orders/components/client.tsx
@@ -18,7 +18,7 @@ export const OrderClient: React.FC<OrderClientProps> = ({
         description="Manage orders for your store"
       />
       <Separator />
-      <DataTable searchKey="label" columns={columns} data={data} />
+      <DataTable searchKey="product" columns={columns} data={data} />
     </>
   )
 }
